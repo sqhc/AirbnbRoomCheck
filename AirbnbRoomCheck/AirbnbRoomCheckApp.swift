fe78b61fa7d141ebde33533552e1ddc34025225f
shenqing <shenqin1@msu.edu>
2023-05-10 13:24:21 -0400

    Initial Commit

---
+++ AirbnbRoomCheck/AirbnbRoomCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbRoomCheckApp.swift
+//  AirbnbRoomCheck
+//
+//  Created by 沈清昊 on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbRoomCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
