b8e0bb780b31163909b325858a08512a5ed16be4
shenqing <shenqin1@msu.edu>
2023-05-12 13:26:01 -0400

    Better display for the list of conditions and days in calendar view.

--- AirbnbRoomCheck/View/RoomCalenderView.swift
+++ AirbnbRoomCheck/View/RoomCalenderView.swift
@@ -43,31 +43,54 @@ struct RoomCalenderView_Previews: PreviewProvider {
 
 struct CalendarItem: View{
     var month: CalenderMonth
+    @State var showMonthDays = false
+    @State var showConditions = false
+    
     var body: some View{
         VStack{
             Text("\(month.name ?? ""), \(month.year ?? 0)")
             Text("Price updated at: \(month.dynamic_pricing_updated_at ?? "")")
                 .frame(alignment: .leading)
-            Text("Month days")
-            if let days = month.days{
-                List(days, id: \.date){ day in
-                    Text(day.date ?? "")
-                    Text(day.available ?? false ? "Available" : "Not available")
-                    Text(day.available_for_checkin ?? false ? "Can checkin" : "Can't checkin")
-                    Text("Local price: \(day.price?.local_price_formatted ?? "")")
-                    Text("Natice price: \(day.price?.native_adjusted_price ?? 0) \(day.price?.native_currency ?? "")")
-                }
-                .frame(width: 300, height: 50)
+            
+            Button {
+                showMonthDays.toggle()
+            } label: {
+                Text("Month days")
+                    .foregroundColor(.black)
+                    .font(.headline)
+                    .padding(20)
+                    .background(Color.red.cornerRadius(10))
             }
-            Text("Conditions")
-            if let conditions = month.condition_ranges{
-                List(conditions, id: \.start_date){ condition in
-                    Text("Start: \(condition.start_date ?? "")")
-                    Text("End: \(condition.end_date ?? "")")
-                    Text(condition.conditions?.closed_to_arrival ?? false ? "Close to arrival" : "Open to arrival")
-                    Text(condition.conditions?.closed_to_departure ?? false ? "Close to departure" : "Open to departure")
+            .sheet(isPresented: $showMonthDays) {
+                if let days = month.days{
+                    List(days, id: \.date){ day in
+                        Text(day.date ?? "")
+                        Text(day.available ?? false ? "Available" : "Not available")
+                        Text(day.available_for_checkin ?? false ? "Can checkin" : "Can't checkin")
+                        Text("Local price: \(day.price?.local_price_formatted ?? "")")
+                        Text("Natice price: \(day.price?.native_adjusted_price ?? 0) \(day.price?.native_currency ?? "")")
+                    }
                 }
-                .frame(width: 300, height: 50)
+            }
+            
+            Button {
+                showConditions.toggle()
+            } label: {
+                Text("Conditions")
+                    .foregroundColor(.green)
+                    .font(.headline)
+                    .padding(20)
+                    .background(Color.yellow.cornerRadius(10))
+            }
+            .sheet(isPresented: $showConditions) {
+                if let conditions = month.condition_ranges{
+                    List(conditions, id: \.start_date){ condition in
+                        Text("Start: \(condition.start_date ?? "")")
+                        Text("End: \(condition.end_date ?? "")")
+                        Text(condition.conditions?.closed_to_arrival ?? false ? "Close to arrival" : "Open to arrival")
+                        Text(condition.conditions?.closed_to_departure ?? false ? "Close to departure" : "Open to departure")
+                    }
+                }
             }
         }
     }
